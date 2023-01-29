e132a4aa462f4564c4aa349397886d622afcb9f8
MoonsRainbow <kalmia6863@gmail.com>
2023-01-30 02:40:20 +0900

    Feature/Jun-add-docs

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     author='MoonsRainbow',
     author_email='Moons_Rainbow@kakao.com',
     license='MoonsRainbow',
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
--- src/shasha4/Jun.py
+++ src/shasha4/Jun.py
@@ -8,18 +8,28 @@ from bs4 import BeautifulSoup as bs
 from matplotlib import pyplot as plt
 
 
+# IPython 환경이 아닐 경우, 에러 발생.
 try:
     from IPython.display import clear_output
 except:
+    # 에러 발생 시 패스.
     pass
 
 
+# 전역 변수 선언.
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
 }
 
 
+# 멀티 프로세스 처리 함수
 def get_item(code):
+    """
+    주식 종목의 코드를 입력받고, 주식 종목의 정보를 스크래핑 해옵니다.
+
+    :param code: str, 주식 종목 코드.
+    :return: pandas.DataFrame, 주식 종목 정보.
+    """
     item_auto_cols = {
         'cd': '종목 코드',
         'nm': '종목 명',
@@ -50,7 +60,14 @@ def get_item(code):
 
 
 class ControlTower:
+    """
+    스크래핑 해온 주식 종목들을 관리합니다.
+    """
+
     def __init__(self):
+        """
+        생성자 메서드
+        """
         self.__upjong_uri = ('get', 'https://finance.naver.com/sise/sise_group.naver?type=upjong')
         self.__items_uri = 'https://finance.naver.com/sise/sise_group_detail.naver?type=upjong&no={}'
         self.__upjong = self.get_upjong()
@@ -59,17 +76,25 @@ class ControlTower:
 
     @property
     def upjong(self):
+        """:return: 업종 정보."""
         return self.__upjong
 
     @property
     def upjong_items(self):
+        """:return: 업종에 해당하는 주식 종목들."""
         return self.__upjong_items
 
     @property
     def plot_items(self):
+        """:return: 그래프 생성에 필요한 정보들."""
         return self.__plot_items
 
     def get_upjong(self):
+        """
+        업종 정보들을 스크래핑 해옵니다.
+
+        :return: pd.DataFrame, 업종 정보.
+        """
         resp = request(*self.__upjong_uri, headers=HEADERS)
         df = pd.read_html(resp.text)[0]
         df.columns = [col[1] for col in list(df.columns)]
@@ -81,12 +106,25 @@ class ControlTower:
         return df
 
     def get_upjong_items(self, upjong_code):
+        """
+        업종에 해당하는 주식 종목의 코드들을 스크래핑 해옵니다.
+
+        :param upjong_code: str, 업종 코드.
+        :return: dict, 업종에 해당하는 종목 명, 종목 코드 딕셔너리.
+        """
         if upjong_code not in self.__upjong_items:
             resp = request('get', self.__items_uri.format(upjong_code), headers=HEADERS)
             self.__upjong_items[upjong_code] = {tag_a.text: tag_a['href'].split('=')[-1] for tag_a in bs(resp.text, 'html.parser').select('div.name_area > a')}
         return self.__upjong_items[upjong_code]
 
     def get_items_auto(self, upjong_code):
+        """
+        주식 종목들의 정보를 8초 단위로 스크래핑 해옵니다.
+
+        스크래핑 해온 정보들을 저장하고, 데이터프레임으로 만들어 디스플레이합니다.
+
+        :param upjong_code: str, 업종 코드.
+        """
         print('데이터를 불러오는 중 입니다...')
         for i in range(10):
             pool = Pool(processes=len(self.__upjong_items[upjong_code]))
@@ -110,9 +148,14 @@ class ControlTower:
                 display(pd.concat(item_auto_datas, ignore_index=True))
             except:
                 print('N')
-            time.sleep(2)
+            time.sleep(8)
 
     def show_plot(self, item_code):
+        """
+        수집된 정보들로 8초봉 차트를 그립니다.
+
+        :param item_code: str, 주식 종목 코드.
+        """
         if item_code not in self.__plot_items:
             print('수집하지 않은 코드입니다.')
         else:
